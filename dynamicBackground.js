7683a929804f301731ab59d7e5b8d7454358b33b
JoffduPreez <joffre@joffredupreez.com>
2023-01-02 20:32:31 -0500

    setup front page and about page

---
+++ dynamicBackground.js
@@ -0,0 +1,13 @@
+VANTA.NET({
+    el: "#header",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: "#84a98c",
+    backgroundColor: "#2f3e46",
+    showDots: false
+})
