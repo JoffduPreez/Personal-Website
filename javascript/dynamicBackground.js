ce305644b688ed5e778d130c25f21a7721a47e30
JoffduPreez <joffre@joffredupreez.com>
2023-01-11 20:01:02 -0500

    changed color scheme, added box shadows

--- javascript/dynamicBackground.js
+++ javascript/dynamicBackground.js
@@ -7,8 +7,8 @@ VANTA.NET({
     minWidth: 200.00,
     scale: 1.00,
     scaleMobile: 1.00,
-    color: "#84a98c",
-    backgroundColor: "#2f3e46",
+    color: "#552cbf",
+    backgroundColor: "#f4f4f4",
     showDots: false
 })
 
@@ -31,8 +31,7 @@ VANTA.FOG({
     gyroControls: false,
     minHeight: 200.00,
     minWidth: 200.00,
-    highlightColor: "#2f3e46",
-    midtoneColor: "#354f52",
-    highlightColor: "#415b5e",
-    lowlightColor: "#84a98c",
+    highlightColor: "#f4f4f4",
+    midtoneColor: "#552cbf",
+    lowlightColor: "#f4f4f4",
 })
