df348c666741554192e587975efb95558be9fc18
JoffduPreez <joffre@joffredupreez.com>
2023-09-10 16:00:24 -0400

    added most relavent info

--- javascript/dynamicBackground.js
+++ javascript/dynamicBackground.js
@@ -12,17 +12,6 @@ VANTA.NET({
     showDots: false
 })
 
-// VANTA.WAVES({
-//     el: "#background",
-//     mouseControls: true,
-//     touchControls: true,
-//     gyroControls: false,
-//     minHeight: 200.00,
-//     minWidth: 200.00,
-//     scale: 1.00,
-//     scaleMobile: 1.00,
-//     color: "#2f3e46",
-// })
 
 VANTA.FOG({
     el: "#background",
